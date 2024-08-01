6e389c18627163f584aa270fbb36c79d3dfad843
Benjamin Daschel <ben@lunarbyte.io>
2024-08-01 15:53:14 -0700

    use stream and pipeline to write response direct to file

--- src/controllers/ExampleController.ts
+++ src/controllers/ExampleController.ts
@@ -1,6 +1,8 @@
 import {Controller} from "@tsed/di";
 import {Get} from "@tsed/schema";
 import axios from "axios";
+import { createWriteStream } from "node:fs";
+import { pipeline } from "node:stream/promises";
 
 
 @Controller('/example')
@@ -9,10 +11,16 @@ export class ExampleController {
 
     @Get('/')
     async getExample() {
-        const response = await axios.get('http://proxy/randomjunk.gz', {
-            responseType: 'arraybuffer',
-            maxContentLength: Infinity,
+        const response = await axios.get<NodeJS.ReadableStream>('http://proxy/randomjunk.gz', {
+            responseType: 'stream',
         });
+
+        const timestamp = Date.now();
+        await pipeline(
+            response.data,
+            createWriteStream(`/tmp/archive_${timestamp}.tar.gz`),
+        );
+        console.log('Pipeline succeeded.');
 
         return 'hello world'
     }
